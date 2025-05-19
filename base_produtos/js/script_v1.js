d9baf5140886a56aa06e465614f1a97ddb7efbaa
GabrielCardoso76 <torresgbriel8@gmail.com>
2025-05-19 17:28:19 -0300

    aula 19/05

---
+++ base_produtos/js/script_v1.js
@@ -0,0 +1,35 @@
+// prettier-ignore
+let produtos = [
+  { nome: 'Fone', diretorio: 'fone', textoDescritivo: 'Fone de Ouvido Philips com Microfone - Branco', imagens: ['01.jpg', '02.jpg', '03.jpg'] },
+  { nome: 'Mouse', diretorio: 'mouse', textoDescritivo: 'Mouse Gamer Acer Nitro 7200 DPI - Preto', imagens: ['01.jpg', '02.jpg', '03.jpg'] },
+  { nome: 'HeadSet', diretorio: 'headSet', textoDescritivo: 'Headset Gamer Com Mic Quantum Jbl  - Preto', imagens: ['01.jpg', '02.jpg', '03.jpg'] },
+  { nome: 'Teclado', diretorio: 'teclado', textoDescritivo: 'Teclado Mecânico Gamer Phantom, ABNT2 - Preto', imagens: ['01.jpg', '02.jpg', '03.jpg'] },
+];
+
+function inicializarProdutos() {
+  let containerPrincipal = document.getElementById('produtos');
+
+  if (containerPrincipal instanceof HTMLDivElement) {
+    produtos.forEach((produto) => {
+      const containerProduto = document.createElement('div');
+
+      if (!(containerProduto instanceof HTMLDivElement)) return;
+
+      containerProduto.classList.add('container');
+
+      produto.imagens.forEach((imagemNome) => {
+        const imagem = document.createElement('img');
+
+        if (imagem instanceof HTMLImageElement) {
+          imagem.src = `./img/${produto.diretorio}/${imagemNome}`;
+          imagem.alt = produto.nome;
+          containerProduto.appendChild(imagem);
+        }
+      });
+
+      containerPrincipal.appendChild(containerProduto);
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', inicializarProdutos);
